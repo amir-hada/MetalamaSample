35b082bef72b51a3eebaa3b4ac27cddf271e79e9
amo-hada <amirhdv83@gmail.com>
2025-04-13 11:48:53 +0330

    after addding the unversioned file and editing the git ignore

---
+++ MySample/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Product product = new Product();
+        
+        product.PropertyChanged += (sender, e) =>
+        {
+            Console.WriteLine($"Property {e.PropertyName} changed.");
+        };
+        
+        product.Title = "Iphone";
+        product.Price = 29.99m;
+        
+    }
+});
